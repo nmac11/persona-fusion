1f679e1c54578c121ba2f8ef2ccd6236bdac65dd
Normar Macawile <nmacawile@gmail.com>
2020-05-24 13:54:32 +0800

    Add app home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   {
     path: 'p3p',
     loadChildren: () => import('./p3p/p3p.module').then((m) => m.P3PModule),
