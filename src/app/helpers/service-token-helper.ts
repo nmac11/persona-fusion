77382b73b3c871bdcedd87c23e8bb9a7bf3f06e8
Normar Macawile <nmacawile@gmail.com>
2020-06-15 07:07:39 +0800

    Organize imports

--- src/app/helpers/service-token-helper.ts
+++ src/app/helpers/service-token-helper.ts
@@ -1,20 +1,22 @@
 import { P3P_COMPENDIUM } from '../tokens/p3p/compendium-service-token';
 import { P3P_NORMAL_FUSION } from '../tokens/p3p/normal-fusion-service-token';
 import { P3P_TRIANGLE_FUSION } from '../tokens/p3p/triangle-fusion-service-token';
+import { P3P_FUSION_CHART } from '../tokens/p3p/fusion-chart-service-token';
+import { P3P_SIMULATOR } from '../tokens/p3p/simulator-service-token';
+import { P3FES_SIMULATOR } from '../tokens/p3fes/simulator-service-token';
+import { P3P_SKILL } from '../tokens/p3p/skill-service-token';
+
 import { P3FES_COMPENDIUM } from '../tokens/p3fes/compendium-service-token';
 import { P3FES_NORMAL_FUSION } from '../tokens/p3fes/normal-fusion-service-token';
 import { P3FES_TRIANGLE_FUSION } from '../tokens/p3fes/triangle-fusion-service-token';
+import { P3FES_FUSION_CHART } from '../tokens/p3fes/fusion-chart-service-token';
+import { P3FES_SKILL } from '../tokens/p3fes/skill-service-token';
+
 import { P4G_COMPENDIUM } from '../tokens/p4g/compendium-service-token';
 import { P4G_NORMAL_FUSION } from '../tokens/p4g/normal-fusion-service-token';
 import { P4G_TRIANGLE_FUSION } from '../tokens/p4g/triangle-fusion-service-token';
-import { P3P_FUSION_CHART } from '../tokens/p3p/fusion-chart-service-token';
-import { P3FES_FUSION_CHART } from '../tokens/p3fes/fusion-chart-service-token';
 import { P4G_FUSION_CHART } from '../tokens/p4g/fusion-chart-service-token';
-import { P3P_SIMULATOR } from '../tokens/p3p/simulator-service-token';
-import { P3FES_SIMULATOR } from '../tokens/p3fes/simulator-service-token';
 import { P4G_SIMULATOR } from '../tokens/p4g/simulator-service-token';
-import { P3P_SKILL } from '../tokens/p3p/skill-service-token';
-import { P3FES_SKILL } from '../tokens/p3fes/skill-service-token';
 import { P4_SKILL } from '../tokens/p4/skill-service-token';
 
 export const serviceToken: any = {
