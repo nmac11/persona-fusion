8416f6f1031b0d50886f92e9c5d7c4d2e0324b81
Normar Macawile <nmacawile@gmail.com>
2020-06-29 16:22:17 +0800

    Create input change debouncer

---
+++ src/app/shared/change-debouncer.directive.ts
@@ -0,0 +1,33 @@
+import {
+  Directive,
+  HostListener,
+  EventEmitter,
+  Output,
+  OnDestroy,
+} from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Directive({
+  selector: '[changeDebouncer]',
+})
+export class ChangeDebouncerDirective implements OnDestroy {
+  @Output() debouncedChange: EventEmitter<string> = new EventEmitter();
+  private subject: Subject<string> = new Subject();
+
+  constructor() {
+    this.subject
+      .pipe(debounceTime(200), distinctUntilChanged())
+      .subscribe((t) => this.debouncedChange.emit(t));
+  }
+
+  ngOnDestroy(): void {
+    this.subject.unsubscribe();
+  }
+
+  @HostListener('change', ['$event.target.value'])
+  onChange(text: string): void {
+    console.log(text)
+    this.subject.next(text);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 import { PersonaInfoComponent } from './persona-info/persona-info.component';
 import { MaterialModule } from '../material/material.module';
 import { InputDebouncerDirective } from './input-debouncer.directive';
+import { ChangeDebouncerDirective } from './change-debouncer.directive';
 
 @NgModule({
-  declarations: [PersonaInfoComponent, InputDebouncerDirective],
+  declarations: [
+    PersonaInfoComponent,
+    InputDebouncerDirective,
+    ChangeDebouncerDirective,
+  ],
   imports: [CommonModule, MaterialModule],
-  exports: [PersonaInfoComponent, InputDebouncerDirective],
+  exports: [
+    PersonaInfoComponent,
+    InputDebouncerDirective,
+    ChangeDebouncerDirective,
+  ],
 })
 export class SharedModule {}
