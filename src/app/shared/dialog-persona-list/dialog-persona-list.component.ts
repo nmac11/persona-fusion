29843a4f38e6f644a843f3c57d54a04c786273e7
Normar Macawile <nmacawile@gmail.com>
2020-07-28 00:13:47 +0800

    Allow skill filtering on dialog persona list

--- src/app/shared/dialog-persona-list/dialog-persona-list.component.ts
+++ src/app/shared/dialog-persona-list/dialog-persona-list.component.ts
@@ -12,6 +12,12 @@ import { SkillService } from '../../services/skill.service';
 import { partialMatchRegExp } from '../../helpers/reg-exp-helpers';
 import { FusionNodeHelper } from '../../simulator/helpers/fusion-node-helper';
 import { ActiveGameService } from '../../services/active-game.service';
+import { Skill } from '../../models/skill';
+
+interface FilteredPersona {
+  persona: Persona;
+  matchedSkills: Skill[];
+}
 
 @Component({
   selector: 'shared-dialog-persona-list',
@@ -26,7 +32,7 @@ export class DialogPersonaListComponent implements OnInit {
   changeSelected: EventEmitter<FusionNode | null> = new EventEmitter();
   @Output() dblClickSelection: EventEmitter<FusionNode> = new EventEmitter();
 
-  list: Persona[];
+  list: FilteredPersona[];
 
   constructor(
     private injector: Injector,
@@ -36,19 +42,44 @@ export class DialogPersonaListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.list = this.compendiumService.getAll();
+    this.loadFromCompendium();
   }
 
   clearFilter(): void {
-    this.list = this.compendiumService.getAll();
+    this.loadFromCompendium();
+  }
+
+  private loadFromCompendium(): void {
+    this.list = this.compendiumService.getAll().map((p) => {
+      return { persona: p, matchedSkills: [] };
+    });
   }
 
   applyFilter(filter: string): void {
-    this.list = this.compendiumService
-      .getAll()
-      .filter((persona: Persona) =>
-        partialMatchRegExp(filter).test(persona.name),
-      );
+    if (filter === '')
+      this.list = this.compendiumService.getAll().map((p) => {
+        return { persona: p, matchedSkills: [] };
+      });
+    else
+      this.list = this.compendiumService.getAll().reduce((res, p: Persona) => {
+        const matchedName = partialMatchRegExp(filter).test(p.name);
+        const matchedSkills = p.skills.filter((s) =>
+          partialMatchRegExp(filter).test(s.name),
+        );
+        if (matchedName) res.unshift({ persona: p, matchedSkills });
+        else if (matchedSkills.length > 0)
+          res.push({ persona: p, matchedSkills });
+        return res;
+      }, []);
+  }
+
+  matchedSkills(skills: Skill[]): string {
+    return skills
+      .map((s) => {
+        const levelSuffix = s.level > 0 ? ` (${s.level})` : '';
+        return s.name + levelSuffix;
+      })
+      .join(', ');
   }
 
   selectionChange(persona: Persona): void {
