61a9e7191f95fe55f4fc3466feb1e892c1a984d6
Normar Macawile <nmacawile@gmail.com>
2020-08-15 17:58:46 +0800

    Remove unnecessary app-db module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { AppDbModule } from './app-db/app-db.module';
 import { AppSettingsComponent } from './components/app-settings/app-settings.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ConfirmClearDatabaseDialogComponent } from './components/confirm-clear-database-dialog/confirm-clear-database-dialog.component';
 import { ReferenceComponent } from './components/reference/reference.component';
 import { ContainerComponent } from './components/container/container.component';
+import { NgxIndexedDBModule } from 'ngx-indexed-db';
+import { appDbConfig } from './config/app-db-config';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,7 @@ import { ContainerComponent } from './components/container/container.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppDbModule,
+    NgxIndexedDBModule.forRoot(appDbConfig),
     ReactiveFormsModule,
   ],
   providers: [Title],
