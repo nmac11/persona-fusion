5cd4119959a92ca268da95043acb5de61ef492c4
Normar Macawile <nmacawile@gmail.com>
2020-08-16 09:26:39 +0800

    Fix sidebar links not highlighted when a game is active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,31 +14,31 @@ export class AppComponent {
       title: 'Home',
     },
     {
-      url: '/p3p/personae',
+      url: '/p3p',
       title: 'Persona 3 Portable',
     },
     {
-      url: '/p3fes/personae',
+      url: '/p3fes',
       title: 'Persona 3 FES: The Journey',
     },
     {
-      url: '/p3ans/personae',
+      url: '/p3ans',
       title: 'Persona 3 FES: The Answer',
     },
     {
-      url: '/p4/personae',
+      url: '/p4',
       title: 'Persona 4',
     },
     {
-      url: '/p4g/personae',
+      url: '/p4g',
       title: 'Persona 4 Golden',
     },
     {
-      url: '/p5/personae',
+      url: '/p5',
       title: 'Persona 5',
     },
     {
-      url: '/p5r/personae',
+      url: '/p5r',
       title: 'Persona 5 Royal',
     },
     {
